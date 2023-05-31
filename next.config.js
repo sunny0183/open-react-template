2c9e25cfe39b2d406be34d74d6c640c22daedef3
gitName <a@b.c>
2023-05-31 14:21:55 +0100

    ignore type errors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.NODE_ENV === "production"
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.NODE_ENV === "production"
+  },
   output: 'standalone',
 }
 
