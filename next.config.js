906f31f425dbb96a5419316773fd890042bd2a34
gitName <a@b.c>
2023-06-01 18:10:43 +0100

    upgrade from parent

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.NODE_ENV === "production"
